bcf7a6c087fa4d863a75610861d10a50ea8f2c8e
Stefan Vukovic <stefan@Stefans-MacBook-Air.local>
2023-03-31 22:57:07 +0200

    models added

--- anonymous/models.py
+++ anonymous/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+class Tags(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=100)
+
+class Comments(models.Model):
+    commentator = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat/', include('anonymous.urls')),
+    path('', include('anonymous.urls')),
 ]
