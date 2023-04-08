bfeed71791825fef158cdba0941ac3b75c79ddec
Stefan Vukovic <stefan@Stefans-MacBook-Air.local>
2023-04-08 12:42:50 +0200

    messages displayed

--- anonymous/views.py
+++ anonymous/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import authenticate, logout
 from django.contrib.auth import login as auth_login
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Anonymous, Post
 from .forms import RegisterForm, NewPost
 import shortuuid
@@ -10,23 +12,30 @@ import shortuuid
 
 def index(request):
     id = shortuuid.ShortUUID().random(length=6)
-    print(id)
-    return render(request, 'anonymous/index.html')
+    get_all_posts = Post.objects.all()
+    context = {'get_all_posts':get_all_posts}
 
+    return render(request, 'anonymous/index.html', context)
+
+@login_required
 def room(request, room_name):
     return render(request, 'anonymous/chatroom.html', {'room_name':room_name})
 
+@login_required
 def new_post(request):
-    if request.method == 'POST':
-        form = NewPost(request.POST)
-        if form.is_valid():
-            title =  form.cleaned_data["title"]
-            description =  form.cleaned_data["description"]
-            user = request.user
-            Post.objects.create(title = title, description = description, user = user)
-    form = NewPost()
-    context = {'form':form}
-    return render(request, 'anonymous/new_post.html', context)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = NewPost(request.POST)
+            if form.is_valid():
+                title =  form.cleaned_data["title"]
+                description =  form.cleaned_data["description"]
+                user = request.user
+                Post.objects.create(title = title, description = description, user = user)
+        form = NewPost()
+        context = {'form':form}
+        return render(request, 'anonymous/new_post.html', context)
+    else:
+       return render(request, 'anonymous/login.html', context)
 
 def login(request):
     if request.method == 'POST':
@@ -36,10 +45,14 @@ def login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             auth_login(request, user)
-            return render(request, 'anonymous/index.html')
+            messages.success(request,"You are logged in successfully!")
+            context = {'green':'green'}
+            return render(request, 'anonymous/index.html', context)
         else:
-            pass # dispaly message
+            messages.error(request,"Incorrect username or password. Please try again.")
+            return render(request, 'anonymous/login.html')
     return render(request, 'anonymous/login.html')
+    
 
 def register(request):
     form = RegisterForm()
--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from django.contrib.messages import constants as messages
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -16,6 +17,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
 
 # Application definition
 
