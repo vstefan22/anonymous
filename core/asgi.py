309b1dbde4666a1ef911805c133c2bcd8bb34473
Stefan Vukovic <stefan@Stefans-MacBook-Air.local>
2023-08-26 12:10:07 +0200

    deployment bug fixing

--- core/asgi.py
+++ core/asgi.py
@@ -7,7 +7,7 @@ from django.core.asgi import get_asgi_application
 
 from anonymous.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
